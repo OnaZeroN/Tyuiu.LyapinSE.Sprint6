bab85d9429b80ea5fba775c662e970b9a59dc61c
Zeron <dotdot20072607@gmail.com>
2025-12-12 03:57:45 +0500

    Add project files.

---
+++ Tyuiu.LyapinSE.Sprint6.Task0.V23.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.LyapinSE.Sprint6.Task0.V23.Lib
+{
+    public class DataService : ISprint6Task0V23
+    {
+        public double Calculate(int x)
+        {
+            double res = Math.Log(x + 1 / x + 2);
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.LyapinSE.Sprint6.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.LyapinSE.Sprint6.Task0.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
