1cea5218f6b6c0f00a27c4a5b0a359c2cddd2ade
Zeron <dotdot20072607@gmail.com>
2025-12-12 06:24:54 +0500

    Task5

---
+++ Tyuiu.LyapinSE.Sprint6.Task5.V2/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.LyapinSE.Sprint6.Task5.V2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
