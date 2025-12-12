e92e3db7966b3a0f9c101e65fb35cfd998610fc7
Zeron <dotdot20072607@gmail.com>
2025-12-12 06:01:17 +0500

    Task3

---
+++ Tyuiu.LyapinSE.Sprint6.Task3.V28.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿
+
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.LyapinSE.Sprint6.Task3.V28.Lib
+{
+    public class DataService
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] % 2 == 0 && i == 3)
+                    {
+                        matrix[i, j] = 0;
+
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.LyapinSE.Sprint6.Task3.V28.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.LyapinSE.Sprint6.Task3.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
