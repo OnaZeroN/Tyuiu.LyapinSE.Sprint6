c2792f4be616533060f1856385d1ebbff268204a
Zeron <dotdot20072607@gmail.com>
2025-12-12 05:23:53 +0500

    Task1

---
+++ Tyuiu.LyapinSE.Sprint6.Task1.V25.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.LyapinSE.Sprint6.Task1.V25.Lib
+{
+    public class DataService : ISprint6Task1V25
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] mass;
+            int len = stopValue - startValue + 1;
+            mass = new double[len];
+            double y;
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                y = (5 * i + 2.5) / (Math.Sin(i) - 2.0) + 2;
+                mass[count] = Math.Round(y, 2);
+                count++;
+
+            }
+            return mass;
+        }
+    }
+}
